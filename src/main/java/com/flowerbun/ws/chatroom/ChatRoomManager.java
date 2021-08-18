d4b0880cf6602d09b34a137e92f0ba1b3a3adcc0
kangmin-gi <bming0227@gmail.com>
2021-08-18 23:40:52 +0900

    chat room 변경

--- src/main/java/com/flowerbun/ws/chatroom/ChatRoomManager.java
+++ src/main/java/com/flowerbun/ws/chatroom/ChatRoomManager.java
@@ -1,6 +1,5 @@
 package com.flowerbun.ws.chatroom;
 
-import com.flowerbun.ws.chatmessage.ChatMessage;
 import com.flowerbun.ws.exception.Common500Exception;
 import com.flowerbun.ws.exception.Errors;
 import java.util.ArrayList;
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.socket.WebSocketSession;
 
 @Component
 public class ChatRoomManager {
@@ -30,22 +28,11 @@ public class ChatRoomManager {
     return this.chatRoomMap.get(roomId);
   }
 
-  public ChatRoom joinRoom(WebSocketSession session, ChatMessage chatMessage) {
-    String roomId = chatMessage.getRoomId();
-    ChatRoom room = this.findRoom(roomId);
-    room.joinUser(session, chatMessage);
-    return room;
-  }
-
   public ChatRoom createRoom(String name) {
     String roomId = this.roomIdGenerator.roomId();
     ChatRoom chatRoom = new ChatRoom(roomId, name);
     this.addRoom(chatRoom);
     return chatRoom;
-  }
-
-  public boolean isChatUser(WebSocketSession session, ChatRoom chatRoom) {
-    return chatRoom.existUser(session);
   }
 
   private void addRoom(ChatRoom chatRoom) {
@@ -55,11 +42,9 @@ public class ChatRoomManager {
   }
 
   private void validRoomId(String roomId) {
-
     if (ObjectUtils.isEmpty(roomId)) {
       Common500Exception.throwException(Errors.EMPTY_ROOM_ID);
     }
-
     if (chatRoomMap.containsKey(roomId)) {
       Common500Exception.throwException(Errors.CREATE_EXIST_ROOM_ID);
     }
